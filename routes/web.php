fa7a75fe32563bb41eadfcc8882467d97493c4d9
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2025-02-21 20:35:40 +0100

    Added routes in middleware

--- routes/web.php
+++ routes/web.php
@@ -5,22 +5,18 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\CommentController;
 use Illuminate\Support\Facades\Route;
 
-// All posts
-Route::get('/', [PostController::class, 'index'])->name('home');
-
-// Post CRUD operations
-Route::resource('posts', PostController::class);
-
-// Comment Routes
-Route::post('/posts/{post}/comments', [CommentController::class, 'store'])->name('comments.store');
-Route::delete('/comments/{comment}', [CommentController::class, 'destroy'])->name('comments.destroy');
-
-// Authenticated users only
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
+    // All posts
+    Route::get('/', [PostController::class, 'index'])->name('home');
+
+    // Post CRUD operations
+    Route::resource('posts', PostController::class);
+
+    // Comment Routes
+    Route::post('/posts/{post}/comments', [CommentController::class, 'store'])->name('comments.store');
+    Route::delete('/comments/{comment}', [CommentController::class, 'destroy'])->name('comments.destroy');
+
+    // Profile Routes
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
